e81aa2934a333cefb7641a20850b28b48e88b4a2
positif thinking aja :) <57127202+najarnaj@users.noreply.github.com>
2019-11-16 08:07:56 +0700

    Add files via upload

---
+++ pembayaran.php
@@ -0,0 +1,34 @@
+<?php
+include("koneksi.php");
+if(isset($_POST['simpan'])){
+	$query_tambah_data="insert into pem_bayaran(merek,qty,harga) values
+	('".$_POST['Merek']."',
+	 '".$_POST['Qty']."',
+	 '".$_POST['Harga']."')";
+	 $proses_data=mysqli_query($rumah,$query_tambah_data);
+	 if ($proses_data){
+		 echo ' Tambah data berhasil<br/>';
+	 } else {
+		 echo mysqli_error();
+	 }
+}	 
+?>
+<form method="POST" action="">
+<table border="4">
+ <tr>
+     <td>nama barang </td>
+     <td><input name="Merek" type="text"></td>
+ </tr>
+  <tr>
+     <td>qty </td>
+     <td><input name="Qty" type="number"></td>
+ </tr>
+  <tr>
+     <td>harga </td>
+     <td><input name="Harga" type="number"></td>
+ </tr>
+ <tr>
+     <td><input name="simpan" type="submit"></td>
+ </tr>
+ </table>
+ </form>
